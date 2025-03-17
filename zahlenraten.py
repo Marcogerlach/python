0c965389a6031ab24391ac538b2b608785af5152
Marcogerlach <marcogerlach04@gmail.com>
2025-03-17 14:53:14 +0100

    add zahlenraten

---
+++ zahlenraten.py
@@ -0,0 +1,36 @@
+import random
+
+option = "ja"
+while option.lower() == "ja":
+    
+    eingaben = []
+    zahl = random.randint(1, 100)
+    while True:
+        try:
+            print(f"Deine bisherigen eingaben sind {eingaben}")
+            eingabe = int(input("Gebe eine Zahl zwischen 1 und 100 ein: "))
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+            continue
+        if eingabe > 100:
+            print(f"{eingabe} ist zu groß")
+            continue
+        elif eingabe < 1:
+            print(f"{eingabe} ist zu klein")
+            continue
+        if eingabe in eingaben:
+            print(f"{eingabe} hast du schon eingegeben.")
+            continue
+
+        eingaben.append(eingabe)
+        if zahl == eingabe:
+            print(f"{zahl} ist richtig und du hast folgende Zahlen falsch geraten: {len(eingaben) - 1}")
+            break
+        elif zahl < eingabe:
+            print("Die gesuchte Zahl ist kleiner.")
+        elif zahl > eingabe:
+            print("Die gesuchte Zahl ist größer.")
+    option = input("Willst du noch eine Runde spielen? (ja/nein)")
+    if option.lower() == "nein":
+        print("Danke fürs Spielen!")
+
