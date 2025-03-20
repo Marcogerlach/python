1c6f832891ecd0fa450975a50bffa9a3a8f1c5d9
Marcogerlach <marcogerlach04@gmail.com>
2025-03-20 09:56:52 +0100

    add modi

--- zahlenraten.py
+++ zahlenraten.py
@@ -23,8 +23,41 @@ while option.lower() == "ja":
         if inhalt:
             durchschnitt = float(ins) / float(len(inhalt))
         print("durschnittliche Versuche: " + str(durchschnitt))
+    
+    modi = input("Willst du einen leichten Modus(0), einen Normalen Modus(1), oder einen Schweren Modus(2)")
+    while modi == 0:
+        try:
+            print(f"Deine bisherigen eingaben sind {eingaben}")
+            eingabe = int(input("Gebe eine Zahl zwischen 1 und 50 ein: "))
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+            continue
+        if eingabe > 50:
+            print(f"{eingabe} ist zu groß")
+            continue
+        elif eingabe < 1:
+            print(f"{eingabe} ist zu klein")
+            continue
+        if eingabe in eingaben:
+            print(f"{eingabe} hast du schon eingegeben.")
+            continue
 
-    while True:
+        eingaben.append(eingabe)
+        if zahl == eingabe:
+            print(f"{zahl} ist richtig und du hast folgende Zahlen falsch geraten: {len(eingaben) - 1}")
+            #statistik = len(eingaben)
+            #inhalt.append(" " + str(statistik))
+            #neuspeichern(" ".join(inhalt))
+            break
+        elif zahl < eingabe:
+            print("Die gesuchte Zahl ist kleiner.")
+        elif zahl > eingabe:
+            print("Die gesuchte Zahl ist größer.")
+    option = input("Willst du noch eine Runde spielen? (ja/nein)")
+    if option.lower() == "nein":
+        print("Danke fürs Spielen!")
+    eingaben = []
+    while modi == 1:
         try:
             print(f"Deine bisherigen eingaben sind {eingaben}")
             eingabe = int(input("Gebe eine Zahl zwischen 1 und 100 ein: "))
@@ -55,4 +88,38 @@ while option.lower() == "ja":
     option = input("Willst du noch eine Runde spielen? (ja/nein)")
     if option.lower() == "nein":
         print("Danke fürs Spielen!")
+    eingaben = []
+
+    while modi == 2:
+        try:
+            print(f"Deine bisherigen eingaben sind {eingaben}")
+            eingabe = int(input("Gebe eine Zahl zwischen 1 und 200 ein: "))
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+            continue
+        if eingabe > 200:
+            print(f"{eingabe} ist zu groß")
+            continue
+        elif eingabe < 1:
+            print(f"{eingabe} ist zu klein")
+            continue
+        if eingabe in eingaben:
+            print(f"{eingabe} hast du schon eingegeben.")
+            continue
+
+        eingaben.append(eingabe)
+        if zahl == eingabe:
+            print(f"{zahl} ist richtig und du hast folgende Zahlen falsch geraten: {len(eingaben) - 1}")
+            #statistik = len(eingaben)
+            #inhalt.append(" " + str(statistik))
+            #neuspeichern(" ".join(inhalt))
+            break
+        elif zahl < eingabe:
+            print("Die gesuchte Zahl ist kleiner.")
+        elif zahl > eingabe:
+            print("Die gesuchte Zahl ist größer.")
+    option = input("Willst du noch eine Runde spielen? (ja/nein)")
+    if option.lower() == "nein":
+        print("Danke fürs Spielen!")
+    eingaben = []
 
