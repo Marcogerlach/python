a24aafa4ff04fce96b6543da779dc20cc02da2b3
Marcogerlach <marcogerlach04@gmail.com>
2025-03-20 12:57:25 +0100

    add Zeitmodus

--- zahlenraten.py
+++ zahlenraten.py
@@ -1,6 +1,6 @@
 import json
 import random
-
+import time
 
 
 def neuspeichern(stats):
@@ -12,7 +12,7 @@ def auslesen():
         with open("Statistik.txt", "r") as file:
             stats = json.load(file)
     except (FileNotFoundError, json.JSONDecodeError):
-        stats = {"0": [], "1": [], "2": []}  
+        stats = {"0": [], "1": [], "2": [], "z0": [], "z1" : [], "z2" : []}  
     return stats
 
 
@@ -41,42 +41,104 @@ while option.lower() == "ja":
             print("Durchschnittliche Versuche modi Schwer: " + str(durchschnitt))
         else:
             print("Keine Statistik vorhanden.")
-    
-    modi = input("Willst du einen leichten Modus(0), einen normalen Modus(1), oder einen schweren Modus(2)? ")
-    grenze = 0
-    if modi == "0":
-        zahl = random.randint(1, 50)
-        grenze = 50
-    elif modi == "1":
-        zahl = random.randint(1, 100)
-        grenze = 100
-    else:
-        zahl = random.randint(1, 200)
-        grenze = 200
-    while True:
-        try:
-            print(f"Deine bisherigen Eingaben sind {eingaben}")
-            eingabe = int(input(f"Gebe eine Zahl zwischen 1 und {grenze} ein: "))
-        except ValueError:
-            print("Bitte eine gültige Zahl eingeben.")
-            continue
-        if eingabe > grenze:
-            print(f"{eingabe} ist zu groß")
-            continue
-        elif eingabe < 1:
-            print(f"{eingabe} ist zu klein")
-            continue
-        if eingabe in eingaben:
-            print(f"{eingabe} hast du schon eingegeben.")
-            continue
+        alle_wertez0 = stats["z0"]
+        if alle_wertez0:
+            durchschnitt = sum(alle_wertez0) / len(alle_wertez0)
+            print("Durchschnittliche Zeit im Zeit modi leicht: " + str(durchschnitt))
+        else:
+            print("Keine Statistik vorhanden.")
+        alle_wertez1 = stats["z1"]
+        if alle_wertez1:
+            durchschnitt = sum(alle_wertez1) / len(alle_wertez1)
+            print("Durchschnittliche Zeit im Zeit modi Normal: " + str(durchschnitt))
+        else:
+            print("Keine Statistik vorhanden.")
+        alle_wertez2 = stats["z2"]
+        if alle_wertez2:
+            durchschnitt = sum(alle_wertez2) / len(alle_wertez2)
+            print("Durchschnittliche Zeit im Zeit modi Schwer: " + str(durchschnitt))
+        else:
+            print("Keine Statistik vorhanden.")
 
-        eingaben.append(eingabe)
-        if zahl == eingabe:
-            print(f"{zahl} ist richtig und du hast folgende Zahlen falsch geraten: {len(eingaben) - 1}")
-            stats[modi].append(len(eingaben))
-            neuspeichern(stats)
-            break
-        elif zahl < eingabe:
-            print("Die gesuchte Zahl ist kleiner.")
-        elif zahl > eingabe:
-            print("Die gesuchte Zahl ist größer.")
+    obermodi = input("Zeitmodus(0)/Normal(1)")
+    if obermodi == "1":
+        modi = input("Willst du einen leichten Modus(0), einen normalen Modus(1), oder einen schweren Modus(2)? ")
+        grenze = 0
+        if modi == "0":
+            zahl = random.randint(1, 50)
+            grenze = 50
+        elif modi == "1":
+            zahl = random.randint(1, 100)
+            grenze = 100
+        else:
+            zahl = random.randint(1, 200)
+            grenze = 200
+        while True:
+            try:
+                print(f"Deine bisherigen Eingaben sind {eingaben}")
+                eingabe = int(input(f"Gebe eine Zahl zwischen 1 und {grenze} ein: "))
+            except ValueError:
+                print("Bitte eine gültige Zahl eingeben.")
+                continue
+            if eingabe > grenze:
+                print(f"{eingabe} ist zu groß")
+                continue
+            elif eingabe < 1:
+                print(f"{eingabe} ist zu klein")
+                continue
+            if eingabe in eingaben:
+                print(f"{eingabe} hast du schon eingegeben.")
+                continue
+
+            eingaben.append(eingabe)
+            if zahl == eingabe:
+                print(f"{zahl} ist richtig und du hast folgende Zahlen falsch geraten: {len(eingaben) - 1}")
+                stats[modi].append(len(eingaben))
+                neuspeichern(stats)
+                break
+            elif zahl < eingabe:
+                print("Die gesuchte Zahl ist kleiner.")
+            elif zahl > eingabe:
+                print("Die gesuchte Zahl ist größer.")
+    elif obermodi == "0":
+        modi = input("Willst du einen leichten Modus(0), einen normalen Modus(1), oder einen schweren Modus(2)? ")
+        grenze = 0
+        if modi == "0":
+            zahl = random.randint(1, 50)
+            grenze = 50
+        elif modi == "1":
+            zahl = random.randint(1, 100)
+            grenze = 100
+        else:
+            zahl = random.randint(1, 200)
+            grenze = 200
+        start = time.time()
+        while True:
+            try:
+                print(f"Deine bisherigen Eingaben sind {eingaben}")
+                eingabe = int(input(f"Gebe eine Zahl zwischen 1 und {grenze} ein: "))
+            except ValueError:
+                print("Bitte eine gültige Zahl eingeben.")
+                continue
+            if eingabe > grenze:
+                print(f"{eingabe} ist zu groß")
+                continue
+            elif eingabe < 1:
+                print(f"{eingabe} ist zu klein")
+                continue
+            if eingabe in eingaben:
+                print(f"{eingabe} hast du schon eingegeben.")
+                continue
+
+            eingaben.append(eingabe)
+            if zahl == eingabe:
+                end = time.time()
+                zeit = end - start
+                print(f"{zahl} ist richtig und du hast folgende Zahlen falsch geraten: {len(eingaben) - 1}, in {zeit:.2f} Sekunden")
+                stats["z" + modi].append(zeit)
+                neuspeichern(stats)
+                break
+            elif zahl < eingabe:
+                print("Die gesuchte Zahl ist kleiner.")
+            elif zahl > eingabe:
+                print("Die gesuchte Zahl ist größer.")
